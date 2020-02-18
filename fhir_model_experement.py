c1f0f293f97dd3216d0c02e9d955ffcaca2afa60
Ilya Beda <ir4y.ix@gmail.com>
2020-02-18 13:58:42 +0700

    Add fhir_model_experement.py

---
+++ fhir_model_experement.py
@@ -0,0 +1,39 @@
+import asyncio
+from fhirpy import AsyncFHIRClient
+from fhirpy.lib import AsyncFHIRResource
+from model.fhirelementfactory import FHIRElementFactory
+
+
+async def update(self, server=None):
+    if server is not None:
+        raise Exception("Can't ovveride server")
+
+    data = self.as_json()
+    r = AsyncFHIRResource(self.client, self.resource_type, **data)
+    await r.save()
+    self.update_with_json(r.serialize())
+    return self
+
+
+def fhir_model_adapter(self, **kwargs):
+    resource_type = kwargs.pop("resource_type")
+    instance = FHIRElementFactory.instantiate(resource_type, kwargs)
+    instance.client = self
+    instance.update = lambda: update(instance)
+    instance.create = lambda: update(instance)
+    return instance
+
+
+async def main():
+    client = AsyncFHIRClient("http://test.fhir.org/r4", authorization="Bearer TOKEN",)
+    client.resource_class = fhir_model_adapter
+
+    p = client.resource("Patient")
+    print(p)
+    await p.create()
+    print(p)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
